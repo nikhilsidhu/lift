37a69b8caaf2bf05cf13b45578c375a09d40de87
Richard Rino <Rich@MacBook-Pro.local>
2022-03-14 16:54:45 -0500

    Setup local mongo db. Set up workouts index view.

--- lift-web/app.js
+++ lift-web/app.js
@@ -2,16 +2,42 @@ const express = require('express');
 const engine = require('ejs-mate');
 const port = 3000;
 const path = require('path');
+const mongoose = require('mongoose');
+const Workout = require('./models/workout');
+
+mongoose.connect('mongodb://localhost:27017/lift');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
 const app = express();
 
 app.engine('ejs', engine);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {ft
+
     res.render('home');
 })
 
+app.get('/workouts', async (req, res) => {
+    const workouts = await Workout.find({});
+    res.render('workouts/idnex');
+
+})
+
+
+
+app.get('/addworkout', async (req, res) => {
+    const workout = new Workout({workoutName: 'Upper #1', workoutDate: 01-01-2000, workoutTime: 2000});
+    await workout.save();
+
+    res.send(workout);
+})
 
 app.listen(port, () => {
     console.log(`Listening on Port ${port}.`);
---
+++ lift-web/models/workout.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// TODO: add workout intensity (rate from 1-5)?
+const WorkoutSchema = new Schema({
+    workoutName: {
+      // name workout (default option too)
+      type: String,
+      required: true,
+    },
+    workoutDate: {
+      // date of workout
+      type: Date,
+      required: true,
+    },
+    workoutTime: {
+      // how long was the whole workout
+      type: Number,
+      required: true,
+    },
+    workoutNote: {
+      // user note about entire workout
+      type: String,
+      required: false,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  });
+
+  module.exports = mongoose.model('Workout', WorkoutSchema);
