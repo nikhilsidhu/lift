400ef69810c170e96e320d5153056b4ddc566b8d
Richard Rino <Rich@MacBook-Pro.local>
2022-04-19 12:20:05 -0500

    refactor: rename keys in workout schema
    Rename workoutName -> name and workoutDate -> date.

--- lift-web/models/workout.js
+++ lift-web/models/workout.js
@@ -4,39 +4,39 @@ const Schema = mongoose.Schema;
 
 // TODO: add workout intensity (rate from 1-5)?
 const WorkoutSchema = new Schema({
-    workoutName: {
-      // name workout (default option too)
-      type: String,
-      required: true,
-    },
-    workoutDate: {
-      // date of workout
-      type: Date,
-      required: true,
-    },
+  name: {
+    // name workout (default option too)
+    type: String,
+    required: true,
+  },
+  date: {
+    // date of workout
+    type: Date,
+    required: true,
+  },
 
-    exercises: [
-      {
-          type: Schema.Types.ObjectId,
-          ref: 'Exercise'
-      }
-    ]
-    
-    /*,
-    workoutTime: {
-      // how long was the whole workout
-      type: Number,
-      required: true,
-    },
-    workoutNote: {
-      // user note about entire workout
-      type: String,
-      required: false,
-    },
-    userId: {
+  exercises: [
+    {
       type: Schema.Types.ObjectId,
-      ref: "User",
-    }, */
-  });
+      ref: 'Exercise'
+    }
+  ]
 
-  module.exports = mongoose.model('Workout', WorkoutSchema);
+  /*,
+  workoutTime: {
+    // how long was the whole workout
+    type: Number,
+    required: true,
+  },
+  workoutNote: {
+    // user note about entire workout
+    type: String,
+    required: false,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  }, */
+});
+
+module.exports = mongoose.model('Workout', WorkoutSchema);
