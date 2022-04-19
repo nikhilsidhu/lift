4fb6b907bea57003b05b9ae4137b932d8f7e16d3
Richard Rino <Rich@MacBook-Pro.local>
2022-04-19 12:42:05 -0500

    refactor: rename key in exercise schema and reflect change in template
    Rename exerciseName -> name. Reflect change in show.ejs template.

--- lift-web/models/exercise.js
+++ lift-web/models/exercise.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-  exerciseName: {
+  name: {
     // name of exercise
     type: String,
     required: true,
@@ -13,22 +13,7 @@ const exerciseSchema = new Schema({
       type: Schema.Types.ObjectId,
       ref: 'Set'
     }
-  ]/* ,
-    sets: {
-      // array of sets completed
-      type: [setSchema],
-      required: false,
-    },
-    exerciseTime: {
-      // for timed exercises
-      type: Number,
-      required: false,
-    },
-    exerciseNote: {
-      // user notes about this exercise
-      type: String,
-      required: false,
-    }, */
+  ]
 });
 
 module.exports = mongoose.model('Exercise', exerciseSchema);
